67c4eed616c03f67715115a023758c829786e036
Thanh Tung <thanhtung.dao2110@gmail.com>
2020-05-20 23:20:15 +0700

    Change model format

--- model.py
+++ model.py
@@ -1,15 +1,12 @@
 from keras.models import Model
-from keras.layers import Input, Add, PReLU, Conv2DTranspose, Concatenate, MaxPooling2D, UpSampling2D, Dropout,concatenate
+from keras.layers import Input, Add
 from keras.layers.convolutional import Conv2D
-from keras.layers.normalization import BatchNormalization
-from keras.callbacks import Callback
 from tensorflow.math import log
 from keras import backend as K
 from tensorflow.logging import set_verbosity, FATAL
 
-# model architecture and loss function
-
 set_verbosity(FATAL)
+
 
 def get_red30_model():
     """
@@ -18,25 +15,41 @@ def get_red30_model():
     Return the model
     """
 
-    inputs = Input(shape=(256,256, 3))
+    inputs = Input(shape=(256, 256, 3))
 
-    enc_conv0 = Conv2D(64, (3, 3), padding="same",activation='relu', kernel_initializer="he_normal")(inputs)
-    short_cut_enc_conv0 = enc_conv0
+    conv1 = Conv2D(
+        64,
+        (3, 3),
+        padding="same",
+        activation="relu",
+        kernel_initializer="he_normal",
+    )(inputs)
+    short_cut_conv1 = conv1
 
-    enc_conv1 = Conv2D(64, (3, 3), dilation_rate = 2, padding="SAME", activation='relu')(enc_conv0)
-    enc_conv2 = Conv2D(64, (3, 3), dilation_rate = 3, padding="SAME", activation='relu')(enc_conv1)
+    conv2 = Conv2D(
+        64, (3, 3), dilation_rate=2, padding="SAME", activation="relu"
+    )(conv1)
+    conv3 = Conv2D(
+        64, (3, 3), dilation_rate=3, padding="SAME", activation="relu"
+    )(conv2)
 
-    enc_conv3 = Add()([enc_conv2, short_cut_enc_conv0])
-    enc_conv3 = Conv2D(64, (3, 3), dilation_rate = 4, padding="SAME", activation='relu')(enc_conv3)
+    conv4 = Add()([conv3, short_cut_conv1])
+    conv4 = Conv2D(
+        64, (3, 3), dilation_rate=4, padding="SAME", activation="relu"
+    )(conv4)
 
-    short_cut_enc_conv3 = enc_conv3
-    enc_conv4 = Conv2D(64, (3, 3), dilation_rate = 3, padding="SAME", activation='relu')(enc_conv3)
-    enc_conv5 = Conv2D(64, (3, 3), dilation_rate = 2, padding="SAME", activation='relu')(enc_conv4)
+    short_cut_conv4 = conv4
+    conv5 = Conv2D(
+        64, (3, 3), dilation_rate=3, padding="SAME", activation="relu"
+    )(conv4)
+    conv6 = Conv2D(
+        64, (3, 3), dilation_rate=2, padding="SAME", activation="relu"
+    )(conv5)
 
-    enc_conv6 = Add()([enc_conv5, short_cut_enc_conv3])
-    enc_conv6 = Conv2D(3, (3, 3), padding='SAME')(enc_conv6)
+    conv7 = Add()([conv6, short_cut_conv4])
+    conv7 = Conv2D(3, (3, 3), padding="SAME")(conv7)
 
-    model = Model(inputs=inputs,outputs=enc_conv6)
+    model = Model(inputs=inputs, outputs=conv7)
     return model
 
 
@@ -46,10 +59,11 @@ def PSNR(y_true, y_pred):
     @param y_pred: predicted value
     """
     max_pixel = 0.5
-    y_pred = K.clip(y_pred, -0.5,0.5)
+    y_pred = K.clip(y_pred, -0.5, 0.5)
     return 10.0 * log((max_pixel ** 2) / (K.mean(K.square(y_pred - y_true))))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     """
     For Debugging purposes
